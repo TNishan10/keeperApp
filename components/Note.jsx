787fb04ff0ff638e3ed93b2bfb3ec1115f6a83aa
NISHAN THAPA <138146815+TNishan10@users.noreply.github.com>
2024-08-11 11:05:55 +0545

    Initial commit

---
+++ components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <body>
+      <div className="note">
+        <h1>This is the title</h1>
+        <p>My name is Nishan Thapa</p>
+      </div>
+    </body>
+  );
+}
+
+export default Note;
